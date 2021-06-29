9b36a917b0566e0ffac369c5d5cf0edb92270c6a
Cyber Labs <32381952+lata-clabs@users.noreply.github.com>
2021-06-29 12:02:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ var css = document.querySelector("h3");
 var body = document.getElementById("body");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
-color1.value =  rgb(0, 128, 128);
-color2.value =  rgb(170, 164, 57);
 function setGradient(){
 	body.style.background= "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 	css.textContent = body.style.background + ";";
